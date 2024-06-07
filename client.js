93c8b5aab1dfed25a77db71abc9efb0f8e413bde
Tom de Visser <tomdevisser29@gmail.com>
2024-06-07 22:13:11 +0200

    Added a popup with styling when clicking the new button

--- client.js
+++ client.js
@@ -4,6 +4,12 @@ TrelloPowerUp.initialize({
       {
         icon: "https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421",
         text: "Estimate Size",
+        callback: function (t) {
+          return t.popup({
+            title: "Estimation",
+            url: "estimate.html",
+          });
+        },
       },
     ];
   },
