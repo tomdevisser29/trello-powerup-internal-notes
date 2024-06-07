b4faf70fcca06f29d2720d195bea81544dfc4881
Tom de Visser <tomdevisser29@gmail.com>
2024-06-07 22:18:32 +0200

    Added the option to store estimates

--- js/estimate.js
+++ js/estimate.js
@@ -1,5 +1,21 @@
 var t = TrelloPowerUp.iframe();
 
+window.estimate.addEventListener("submit", function (event) {
+  event.preventDefault();
+  return t
+    .set("card", "shared", "estimate", window.estimateSize.value)
+    .then(function () {
+      t.closePopup();
+    });
+});
+
 t.render(function () {
-  t.sizeTo("#estimate").done();
+  return t
+    .get("card", "shared", "estimate")
+    .then(function (estimate) {
+      window.estimateSize.value = estimate;
+    })
+    .then(function () {
+      t.sizeTo("#estimate").done();
+    });
 });
